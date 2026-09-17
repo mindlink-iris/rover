d8ddc416d982e6bd1e69a5e146283e11994e6cec
Julie Kuramochi <julie.kuramochi@mindlinksoft.com>
2026-09-17 12:35:26 +0100

    Added initial test to initialise location

--- Rover.Tests/InitialiseRoverTests.cs
+++ Rover.Tests/InitialiseRoverTests.cs
@@ -2,15 +2,22 @@ namespace Rover.Tests
 { 
     public class InitialiseRoverTests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
+        public void InitializingRoverAtPositionAndDirectionInitializesPosition()
         {
-            Assert.Pass();
+            var location = (5, 6);
+            Rover rover = new Rover(location);
+            Assert.That(rover.Location, Is.EqualTo(location));
+        }
+    }
+
+    public class Rover
+    {
+        public (int, int) Location { get; }
+
+        public Rover((int, int) location)
+        {
+            Location = location;
         }
     }
 }
